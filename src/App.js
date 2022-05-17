ffa5dd1343acca1ebe78c4508c60f932dc8419c4
sujitha4 <sujitha@neoito.com>
2022-05-17 12:27:06 +0530

    style: Color added for text that showing for empty search

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App(){
             <button className="button" onClick={handleSubmit}>{loading ? "searching..." :"search"}  </button>
 
           </form>
-          {repos.length==0 && <div>Enter a username and search</div>}
+          {repos.length==0 && <div className="text">Enter a username and search</div>}
           <div className="results-container">
             {repos.map(renderRepo)}
 
